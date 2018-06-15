e19b8694992abccb15d8d562914e8e325b2430e5
Ignacio Gomez <iegomez@manglar.cl>
2018-06-15 10:10:48 -0400

    Removed comments from customer test.

--- customer_test.go
+++ customer_test.go
@@ -14,22 +14,6 @@ func TestCustomer(t *testing.T) {
 	Convey("Given valid token a client should be created", t, func() {
 		c := NewClient(token, true)
 
-		//Create a customer
-		/*
-					//Customer struct to represent a qvo customer object.
-			type Customer struct {
-				ID                   string         `json:"id"`
-				DefaultPaymentMethod Card           `json:"default_payment_method"`
-				Name                 string         `json:"name"`
-				Email                string         `json:"email"`
-				Subscriptions        []Subscription `json:"subscriptions"`
-				Cards                []Card         `json:"cards"`
-				Transactions         []Transaction  `json:"transactions"`
-				CreatedAt            time.Time      `json:"created_at"`
-				UpdatedAt            time.Time      `json:"updated_at"`
-			}
-		*/
-
 		Convey("After listing customers", func() {
 
 			var where = make(map[string]map[string]interface{})
